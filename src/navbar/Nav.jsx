906def8f4d738d133f0966fad1cced3e59410db1
Piyush Goyal <piyush2022ug@gmail.com>
2025-01-18 17:10:38 +0530

    fix: samll fixes

--- src/navbar/Nav.jsx
+++ src/navbar/Nav.jsx
@@ -59,7 +59,7 @@ function Nav() {
             GAMEABLE
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -153,7 +153,7 @@ function Nav() {
                 </MenuItem>
               ))}
             </Menu>
-          </Box>
+          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
